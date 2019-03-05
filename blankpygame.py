6d6a90bfef2c46934314e9c23f783939875f9d40
chris8855 <29038078+chris8855@users.noreply.github.com>
2019-03-05 18:48:02 +0100

    Added comments

--- blankpygame.py
+++ blankpygame.py
@@ -1,14 +1,21 @@
+#importing pygame
 import pygame, sys
 from pygame.locals import *
 
+#initialising pygame
 pygame.init()
+
+#set up game window
 DISPLAYSURF = pygame.display.set_mode((400, 300))
+
+#caption game window
 pygame.display.set_caption('Hello Pygame World!')
-while True: # main game loop
+
+# main game loop
+while True:
 
 
-
-    
+    #IDLE friendly quitting
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
