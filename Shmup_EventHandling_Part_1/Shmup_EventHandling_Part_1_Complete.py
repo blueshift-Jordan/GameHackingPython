6bd0852a9e2405d4b40f69e9a70775e9fd2085aa
Lloyd Harrison <lloydharrison@gmail.com>
2019-03-19 14:29:35 +0000

    Completed code

---
+++ Shmup_EventHandling_Part_1/Shmup_EventHandling_Part_1_Complete.py
@@ -0,0 +1,74 @@
+import pygame
+
+pygame.init()
+
+# Colours
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# Screen settings
+WIDTH = 400
+HEIGHT = 600
+FPS = 30
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+pygame.display.set_caption("Shmup")
+
+done = False
+clock = pygame.time.Clock()
+
+# Player
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((25,25))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH/2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0
+
+    def update(self):
+        self.rect.x += self.speedx
+        if player.rect.left < 0:
+            player.rect.left = 0
+        if player.rect.right > WIDTH:
+            player.rect.right = WIDTH
+
+player = Player()
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
+
+# Game loop
+while not done:
+
+    clock.tick(FPS)
+
+    # Process inputs/events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player.speedx = -5
+            if event.key == pygame.K_RIGHT:
+                player.speedx = 5
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                player.speedx = 0
+            if event.key == pygame.K_RIGHT:
+                player.speedx = 0
+
+    # Draw
+    screen.fill(BLACK)
+
+    all_sprites.update()
+    all_sprites.draw(screen)
+
+    # Flip the display
+    pygame.display.flip()
+
+pygame.quit()
