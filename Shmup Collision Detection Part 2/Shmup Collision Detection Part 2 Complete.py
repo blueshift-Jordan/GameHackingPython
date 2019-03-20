c05306db125bcb2666a9e8a7809cd46ba0024e2d
Lloyd Harrison <lloydharrison@gmail.com>
2019-03-20 11:38:45 +0000

    Shmup Collision Detection Part 2

---
+++ Shmup Collision Detection Part 2/Shmup Collision Detection Part 2 Complete.py
@@ -0,0 +1,103 @@
+import pygame
+import random
+
+pygame.init()
+
+# Colours
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# Screen settings
+WIDTH = 400
+HEIGHT = 600
+FPS = 30
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+pygame.display.set_caption("Shmup")
+
+done = False
+clock = pygame.time.Clock()
+
+# Player
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((25,25))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH/2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0
+
+    def update(self):
+        self.rect.x += self.speedx
+        if player.rect.left < 0:
+            player.rect.left = 0
+        if player.rect.right > WIDTH:
+            player.rect.right = WIDTH
+
+# Enemies
+class Mob(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((25,25))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(WIDTH - self.rect.width)
+        self.rect.y = random.randrange(-100, -40)
+        self.speedy = random.randrange(2, 6)
+
+    def update(self):
+        self.rect.y += self.speedy
+        if self.rect.top > HEIGHT:
+            self.rect.x = random.randrange(WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100, -40)
+            self.speedy = random.randrange(2, 6)
+
+player = Player()
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
+mobs = pygame.sprite.Group()
+for i in range(15):
+    m = Mob()
+    all_sprites.add(m)
+    mobs.add(m)
+
+# Game loop
+while not done:
+
+    clock.tick(FPS)
+
+    # Process inputs/events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player.speedx = -5
+            if event.key == pygame.K_RIGHT:
+                player.speedx = 5
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                player.speedx = 0
+            if event.key == pygame.K_RIGHT:
+                player.speedx = 0
+
+    # Update
+    all_sprites.update()
+
+    hits = pygame.sprite.spritecollide(player, mobs, False)
+    if hits:
+        done = True
+
+    # Draw
+    screen.fill(BLACK)
+    all_sprites.draw(screen)
+
+    # Flip the display
+    pygame.display.flip()
+
+pygame.quit()
