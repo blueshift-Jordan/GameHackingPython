6970172b8453d8383f145b37a591b7e5044761e9
Lloyd Harrison <lloydharrison@gmail.com>
2019-03-20 11:15:21 +0000

    Update Shmup_EventHandling_Part_1.py

--- Shmup_EventHandling_Part_1/Shmup_EventHandling_Part_1.py
+++ Shmup_EventHandling_Part_1/Shmup_EventHandling_Part_1.py
@@ -27,7 +27,9 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-
+    
+    # Update
+            
     # Draw
     screen.fill(BLACK)
 
