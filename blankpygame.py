923f00b83ae4593714560d580c36b91f8de0c9c4
Chris8855 <29038078+chris8855@users.noreply.github.com>
2019-05-23 16:00:26 +0100

    Update blankpygame.py

--- blankpygame.py
+++ blankpygame.py
@@ -5,15 +5,18 @@ from pygame.locals import *
 #initialising pygame
 pygame.init()
 
-#set up game window
-DISPLAYSURF = pygame.display.set_mode((400, 300))
+set up game window
+screen = pygame.display.set_mode((400, 300))
 
 #caption game window
 pygame.display.set_caption('Hello Pygame World!')
 
 # main game loop
 while True:
+    #remember the ___INDENT___    
 
+    # This MUST happen after all the other drawing commands.
+    pygame.display.flip()
 
     #IDLE friendly quitting
     for event in pygame.event.get():
